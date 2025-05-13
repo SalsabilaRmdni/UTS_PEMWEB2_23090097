4465751bd52433ab80ee3917a1c4c3cb2c221b02
SalsabilaRmdni <165672889+SalsabilaRmdni@users.noreply.github.com>
2025-05-13 18:09:09 +0700

    utspemweb2

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,14 @@
+<x-layouts.app :title="__('Posts')">
+    <h1>halaman admin posts</h1>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Title</th>
+                <th>content</th>
+                <th>Created</th>
+                <th>Uptade</th>
+            </tr>
+        </thead>
+    </table>
+</x-layouts.app>
